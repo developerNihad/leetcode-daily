e45d40564fa050bad42aad55540bc546477913bc
Nihad Nahmatli <140307533+developerNihad@users.noreply.github.com>
2025-02-28 18:19:06 +0000

    new commit

---
+++ 7.remove_duplicates_from_sorted_list.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeDuplicates(self, nums):
+
+        if not nums:
+            return 0
+        
+        i = 0
+
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        
+        return i + 1
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+test = Solution()
+k = test.removeDuplicates(nums)
+print(k, nums[:k])
