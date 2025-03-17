aa2231800908e65d43d3091a504ef838eb6e3827
Nihad Nahmatli <140307533+developerNihad@users.noreply.github.com>
2025-03-17 18:07:00 +0000

    new commit

---
+++ practise_dsa.py
@@ -0,0 +1,36 @@
+
+# FizzBuzz Problem:
+
+"""for num in range(1, 21):
+    if (num % 3 == 0) and (num % 5 == 0):
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)"""
+
+# Sum Even Numbers between 1 and n:
+
+"""def sum_even_numbers(n):
+    sum = 0
+    for num in range(1, n + 1):
+        sum += num
+    return sum
+
+print(sum_even_numbers(10))"""
+
+# Counts The Occurrences Of Each Character In A String:
+
+"""def count_characters(string):
+    char_count = {}
+    for char in string:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+print(count_characters("salam"))"""
+
