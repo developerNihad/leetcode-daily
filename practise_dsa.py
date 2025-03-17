cf35dccd1d5ce3c3d24e23ae198bfdd74f7bdcca
Nihad Nahmatli <140307533+developerNihad@users.noreply.github.com>
2025-03-17 19:01:52 +0000

    new commit

--- practise_dsa.py
+++ practise_dsa.py
@@ -13,6 +13,7 @@
 
 # Sum Even Numbers between 1 and n:
 
+
 """def sum_even_numbers(n):
     sum = 0
     for num in range(1, n + 1):
@@ -20,6 +21,7 @@
     return sum
 
 print(sum_even_numbers(10))"""
+
 
 # Counts The Occurrences Of Each Character In A String:
 
@@ -34,3 +36,64 @@ print(sum_even_numbers(10))"""
 
 print(count_characters("salam"))"""
 
+
+# Find The Largest Number In A List:
+
+"""def find_largest_number(numbers):
+
+    if not numbers:
+        return None
+
+    largest = numbers[0]
+
+    for num in numbers:
+        if num > largest:
+            largest = num
+
+    return largest
+
+print(find_largest_number([243, 53, 245]))"""
+
+
+# Second Way (built-in function)
+
+"""def find_largest_number(numbers):
+
+    return max(numbers) if numbers else None
+
+print(find_largest_number([243, 53, 245]))"""
+
+
+# If A String Is A Palindrome
+
+"""def is_palindrome(string):
+
+    string = string.lower()
+
+    return string == string[::-1]"""
+
+
+# Fibonacci Numbers
+
+"""def generate_fibonacci(n):
+
+    if n <= 0:
+        return False
+    
+    elif n == 1:
+        return [0]
+
+    elif n == 2:
+        return [0, 1]
+
+    fibonacci = [0, 1]
+
+    for i in range(2, n):
+        next_number = fibonacci[i-1] + fibonacci[i-2]
+        fibonacci.append(next_number)
+
+    return fibonacci
+
+print(generate_fibonacci(1))
+print(generate_fibonacci(2))
+print(generate_fibonacci(5))"""
